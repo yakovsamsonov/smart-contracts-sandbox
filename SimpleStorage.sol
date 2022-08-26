70e529ff65863cde4789d04dd5ab917ef4603052
Yakov <yasamsonov@gmail.com>
2022-08-26 15:44:25 +0300

    AddPerson function

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,19 +4,20 @@ pragma solidity 0.8.8;
 contract SimpleStorage {
     // main data types: boolean, uint, sting, int, address, bytes
     bool hasFavoriteNumber = true;
-    uint256 public favoriteNumber = 123; //uint means uint256 by default
+    uint256 favoriteNumber = 123; //uint means uint256 by default
     string favoriteNumberInText = "five";
     int256 favoriteInt = -5;
     bytes32 favoriteBites = "cat";
 
     uint256 favoriteNumber2; // initialized with 0
 
-    People public people = People({favoriteNumber: 2, name: "Patrick"});
-
     struct People {
         uint256 favoriteNumber;
         string name;
     }
+
+    //uint256[] public favoriteNumbersList
+    People[] public people;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
@@ -26,6 +27,11 @@ contract SimpleStorage {
     // view - reads blockchain state, pure - doesn't use blockchain at all 
     function retrieve() public view returns(uint256) {
         return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});
+        people.push(newPerson);        
     }
 
     constructor() {
