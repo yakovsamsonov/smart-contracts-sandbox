3520b50fc0fbfcc32e2b6e87372f26ddc9727437
Yakov <yasamsonov@gmail.com>
2022-09-08 18:23:29 +0300

    More gas effecient

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -7,24 +7,28 @@ pragma solidity ^0.8.8;
 
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50 * 1e18;
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public immutable i_owner;
+
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this contract?
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough"); // 1e18 == 1 * 10 ** 18 == 1 ETH
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough"); // 1e18 == 1 * 10 ** 18 == 1 ETH
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
         // 18 decimals
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         /* starting index, ending index, step amount */
         for(uint256 funderIndex=0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
@@ -48,6 +52,12 @@ contract FundMe {
     }
 
     constructor() {
-        
+        i_owner = msg.sender;  
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == i_owner, "Sender is not owner");
+        // if (msg.sender != i_owner) { revert NotOwner(); } more gas effecient
+        _;        
     }
 }
